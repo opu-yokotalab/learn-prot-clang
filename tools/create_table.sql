9df3d6f28a5c1531da73ff766fce1b7be29164fd
learn <learn@172b1ad7-898d-4ec6-9c98-7da01e4142c6>
2008-02-11 19:44:27 +0000

    git-svn-id: file:///Users/kunishi/tmp/svnroot/prot_clang/trunk@41 172b1ad7-898d-4ec6-9c98-7da01e4142c6

--- tools/create_table.sql
+++ tools/create_table.sql
@@ -1,26 +1,27 @@
 create table compile_error (
 error_flag	boolean,
+compile_count	integer,
 line	integer,
 function_name	text,
 source_name	text,
+error_description	text,
 sourcr_rev	text,
 time	timestamp without time zone,
 compile_pkey	serial not null	primary key
 );
 
-create table run_error (
+create table debug_error (
 error_flag	boolean,
+debug_count	integer,
 line	integer,
 function_name	text,
 memory_address	text,
 signal_name	text,
 source_name	text,
+evaluate_result	boolean,
 source_rev	text,
 run_rev	text,
+trace_rev	text,
 time	timestamp without time zone,
 run_pkey	serial not null primary key
-);
-
-create table evaluate_result (
-
 );
